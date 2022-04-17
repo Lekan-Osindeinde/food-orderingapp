9f778cc758601c19d959faf31c73a4762deea191
Lekan Osindeinde <osindeindelekan@gmail.com>
2022-04-17 05:17:21 +0100

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "../styles/navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.phone}>
+          <img src="/img/telephone.png" alt="" className={styles.img} />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW!</div>
+          <div className={styles.text}>0163367675454</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Product</li>
+          <li className={styles.listItem}>Menu</li>
+          <img src="/img/logo.png" alt="" width="50px" height="50px" />
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <img
+            src="/img/cart.png"
+            alt=""
+            width="30px"
+            height="30px"
+            className={styles.imgCart}
+          />
+          <div className={styles.counter}>3</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
